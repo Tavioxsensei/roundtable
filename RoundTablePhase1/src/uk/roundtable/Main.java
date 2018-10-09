24278e0ff40a7ba2a13e6b5208fefffa5f678c49
François TAVIN <francois.tavin@serendip-it.fr>
2018-10-09 10:23:23 +0200

    [Phase 1] Introduction
    Ajout du code pour gérer l'introduction de la table ronde en fonction du
    tableau "chevaliers".
    Cas d'exception géré : aucun chevalier présent.

--- RoundTablePhase1/src/uk/roundtable/Main.java
+++ RoundTablePhase1/src/uk/roundtable/Main.java
@@ -2,7 +2,20 @@ package uk.roundtable;
 
 public class Main {
 
+    public static String[] chevaliers = new String[] {};
+
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        System.out.println("Bonjour à tous.");
+        System.out.println("Je suis Merlin et en l'absence d'Arthur, j'ai pour mission d'animer cette Table Ronde.");
+
+        if (chevaliers.length > 0) {
+            System.out.println("Sont présents aujourd'hui :");
+            for (String chevalier : chevaliers) {
+                System.out.println("- " + chevalier);
+            }
+            System.out.println("Merci de vous être déplacés.");
+        } else {
+            System.out.println("Bon par contre comme personne n'est encore arrivé, je parle dans le vide là...");
+        }
     }
 }
